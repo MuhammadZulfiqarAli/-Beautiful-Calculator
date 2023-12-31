24e9f642e8a61b54c54fc2f919e3bc44e9a650c2
muhammadzulfiqarali <m.zulfiqarali06@gmail.com>
2023-12-31 21:34:28 +0500

    beautiful calculator

---
+++ script.js
@@ -0,0 +1,23 @@
+   var sum =" "
+  function getNumber (num){
+       sum += num
+ var   input =   document.getElementById("input")
+      input.value = sum 
+   }
+
+  function calculate (){
+     var   input =   document.getElementById("input")
+     var value = input.value 
+     var result = eval(value) 
+    input.value = result 
+   
+
+ }
+  function  del (){
+     var   input =   document.getElementById("input")
+    input.value =" Clear" 
+    
+  }
+
+                     
+
